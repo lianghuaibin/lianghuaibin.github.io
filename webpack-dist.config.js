e34c118feb6c3edcb8b06ece0c97042e8a0fc170
chaos <376288755@qq.com>
2023-03-02 18:01:19 +0800

    测试提交

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -7,6 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const EndWebpackPlugin = require('end-webpack-plugin');
 const { WebPlugin } = require('web-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const outputPath = path.resolve(__dirname, 'docs');
 module.exports = {
@@ -39,6 +40,7 @@ module.exports = {
     main: './src/main.js',
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new DefinePlugin({
       'process.env': {
         // 'NODE_ENV': JSON.stringify('production')
@@ -57,10 +59,10 @@ module.exports = {
       // fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'lianghuaibin.github.io');
 
       // 调用 Chrome 渲染出 PDF 文件
-      /*const chromePath = findChrome();
+      const chromePath = findChrome();
       spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
         'http://resume.lhb666.vip' // 这里注意改成你的在线简历的网站
-      ]);*/
+      ]);
     }),
   ]
 };
