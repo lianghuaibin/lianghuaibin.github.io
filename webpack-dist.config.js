6a8dce9b3a3ab9aa66c596c44db6b3235addd563
lianghuaibin <huaibin.liang@geekplus.com>
2024-02-04 11:01:05 +0800

    开启自动打包，关闭pdf自动生成，防止中文乱码

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -59,10 +59,10 @@ module.exports = {
       // fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'lianghuaibin.github.io');
 
       // 调用 Chrome 渲染出 PDF 文件
-      const chromePath = findChrome();
-      spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'http://resume.lhb666.vip' // 这里注意改成你的在线简历的网站
-      ]);
+      // const chromePath = findChrome();
+      // spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
+      //   'http://resume.lhb666.vip' // 这里注意改成你的在线简历的网站
+      // ]);
     }),
   ]
 };
